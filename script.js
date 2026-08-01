d6c5315363428149e318fb3a418495e7c048a7cf
PetesStache <nk8wv8m9z5@privaterelay.appleid.com>
2026-08-01 12:32:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,96 @@ let team = {
 const players = {
 
 qb:[
-"Brandon Weeden",
-"Mason Rudolph",
-"Zac Robinson",
-"Mike Gundy"
-],
+let quarterbacks = [];
+
+let team = {
+    qb:null,
+    rb:null,
+    wr:[],
+    te:null,
+    ol:null,
+    defense:null
+};
+
+
+async function loadQuarterbacks(){
+
+    const response = await fetch("data/quarterbacks.json");
+
+    quarterbacks = await response.json();
+
+    createQBcards();
+
+}
+
+
+function createQBcards(){
+
+    let div = document.getElementById("qb");
+
+    quarterbacks.forEach(player=>{
+
+        let card = document.createElement("div");
+
+        card.className="player-card";
+
+        card.innerHTML=`
+
+        <div class="rating">
+        ${player.overall}
+        </div>
+
+        <h3>${player.name}</h3>
+
+        <p>${player.years}</p>
+
+        <p>${player.tier}</p>
+
+        <div class="stat">
+        Arm Talent
+        <b>${player.visible.armTalent}</b>
+        </div>
+
+        <div class="stat">
+        Accuracy
+        <b>${player.visible.accuracy}</b>
+        </div>
+
+        <div class="stat">
+        Playmaking
+        <b>${player.visible.playmaking}</b>
+        </div>
+
+        <div class="stat">
+        Clutch
+        <b>${player.visible.clutch}</b>
+        </div>
+
+        `;
+
+
+        card.onclick=function(){
+
+            document.querySelectorAll("#qb .player-card")
+            .forEach(x=>x.classList.remove("selected"));
+
+            card.classList.add("selected");
+
+            team.qb=player;
+
+            console.log(team);
+
+        };
+
+
+        div.appendChild(card);
+
+    });
+
+}
+
+
+loadQuarterbacks();
 
 rb:[
 "Barry Sanders",
